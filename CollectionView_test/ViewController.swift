39acbbc096e8d01abcb251cab3a7dfc9fb42257d
Tsubasa Hayashi <yoku.rin.99@gmail.com>
2016-10-20 21:14:23 +0900

    Initial Commit

---
+++ CollectionView_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionView_test
+//
+//  Created by Hayashi Tsubasa on 2016/10/20.
+//  Copyright © 2016年 Hayashi Tsubasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
