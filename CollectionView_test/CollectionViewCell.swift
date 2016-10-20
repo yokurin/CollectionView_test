70ac20b0dd0ef0c8619d8adb2560e3fea471d06d
Tsubasa Hayashi <yoku.rin.99@gmail.com>
2016-10-20 21:40:55 +0900

    add collection view and collection view cell

---
+++ CollectionView_test/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  CollectionView_test
+//
+//  Created by Hayashi Tsubasa on 2016/10/20.
+//  Copyright © 2016年 Hayashi Tsubasa. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- CollectionView_test/ViewController.swift
+++ CollectionView_test/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        self.collectionView.register(nib, forCellWithReuseIdentifier: "CollectionViewCell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        cell.label.text = "test"
+        
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    // set cell size
+    // must set UICollectionViewDelegateFlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSize: CGFloat = self.view.frame.size.width / 2 - 5
 
+        return CGSize(width: cellSize, height: cellSize)
+    }
+    
 }
 
